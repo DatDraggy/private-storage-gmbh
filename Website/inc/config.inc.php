008d84300d8af241433884ecf8482ed34e091ccb
Kieran <bmxstreet@gmx.net>
2018-11-12 08:20:56 +0100

    lel

--- Website/inc/config.inc.php
+++ Website/inc/config.inc.php
@@ -1,6 +1,7 @@
 <?php
-$db_host = 'myadmin.kieran.de';
-$db_name = 'storagegmbh';
-$db_user = 'schule';
-$db_password = 'jaymax552#';
-$pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password);
+$config['db_host'] = 'myadmin.kieran.de';
+$config['db_name'] = 'storagegmbh';
+$config['db_user'] = 'schule';
+$config['db_password'] = 'jaymax552#';
+$config['db_port'] = 3300;
+$config['db_mail'] = 'cumguzzlingdragon@kieran.de';
