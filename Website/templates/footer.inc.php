cb32d7080060aa7299eb2f134042fd7418904eb5
DatDraggy <marlinvk92@gmail.com>
2018-11-19 08:59:59 +0100

    function

--- Website/templates/footer.inc.php
+++ Website/templates/footer.inc.php
@@ -9,7 +9,7 @@
       <div class="pricing row">
         <?php
         require_once(__DIR__ . '/../inc/shared.inc.php');
-        $dbConnection = buildDatabaseConnection();
+        $dbConnection = buildDatabaseConnection($config);
         try{
           $sql = "SELECT raeume.groesse, preis FROM raeume INNER JOIN preise ON raeume.groesse = preise.groesse GROUP BY raeume.groesse";
           $stmt = $dbConnection->prepare("SELECT raeume.groesse, preis FROM raeume INNER JOIN preise ON raeume.groesse = preise.groesse GROUP BY raeume.groesse");
