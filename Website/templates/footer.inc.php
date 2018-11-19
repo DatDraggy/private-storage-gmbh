1ab6352c56f744cfd39d599b6bc8c21be08e653b
DatDraggy <marlinvk92@gmail.com>
2018-11-19 08:49:47 +0100

    test dyn footer gen

--- Website/templates/footer.inc.php
+++ Website/templates/footer.inc.php
@@ -1,76 +1,54 @@
-<?php 
 
-?> 
- <hr>
-  <div class="container">
-    <div class="light-wrapper">
+<hr>
+<div class="container">
+  <div class="light-wrapper">
     <div class="container inner">
       <h3>Sie haben die Wahl von 3 verschiedenen Räumen</h3>
       <p></p>
       <div class="divide20"></div>
       <div class="pricing row">
-        <div class="col-sm-4">
-          <div class="plan">
-            <h3>5m²</h3>
-            <h4><span class="amount"><span>€</span>5</span></h4>
-            <div class="features">
-			   <ul>
-                <li></li>
-                <li></li>
-                <img class="bestellen" src="images/lager.png">
-				<li></li>
-                <li></li>
-              </ul>
-            </div>
-            <div class="select">
-              <div> <a href="#" class="btn-lg btn-primary btn-block1">bestellen</a> </div>
-            </div>
-          </div>
-        </div>
-        <!-- /.col-sm-4  -->
-        <div class="col-sm-4">
-          <div class="plan">
-            <h3> <span class="featured f1"></span>10m²</h3>
-            <h4><span class="amount"><span>€</span>10</span></h4>
-            <div class="features">
-              <ul>
-                <li></li>
-                <li></li>
-                <img class="bestellen" src="images/lager.png">
-                <li></li>
-                <li></li>
-              </ul>
+        <?php
+        require_once('shared.inc.php');
+        $dbConnection = buildDatabasConnection();
+        try{
+          $sql = "SELECT raeume.groesse, preis FROM raeume INNER JOIN preise ON raeume.groesse = preise.groesse GROUP BY raeume.groesse";
+          $stmt = $dbConnection->prepare("SELECT raeume.groesse, preis FROM raeume INNER JOIN preise ON raeume.groesse = preise.groesse GROUP BY raeume.groesse");
+          $stmt->execute();
+          $rows = $stmt->fetchAll();
+        } catch (PDOException $e) {
+          notifyOnException('Database Select', $config, $sql, $e);
+        }
+
+        foreach($rows as $row) {
+          $groesse = $row['groesse'];
+          $preis = $row['groesse'];
+          ?>
+          <div class="col-sm-4">
+            <div class="plan">
+              <h3><?php echo $groesse; ?>m²</h3>
+              <h4><span class="amount"><span>€</span><?php echo $preis; ?></span></h4>
+              <div class="features">
+                <ul>
+                  <li></li>
+                  <li></li>
+                  <img class="bestellen" src="images/lager<?php echo $groesse; ?>.png">
+                  <li></li>
+                  <li></li>
+                </ul>
+              </div>
               <div class="select">
-                <div> <a href="#" class="btn-lg btn-primary btn-block1">bestellen</a> </div>
+                <div><a href="#" class="btn-lg btn-primary btn-block1">Bestellen</a></div>
               </div>
             </div>
           </div>
-        </div>
-        <!-- /.col-sm-4  -->
-        <div class="col-sm-4">
-          <div class="plan">
-            <h3>15m²</h3>
-            <h4><span class="amount"><span>€</span>15</span></h4>
-            <div class="features">
-              <ul>
-                <li></li>
-                <li></li>
-                <img class="bestellen" src="images/lager.png">
-                <li></li>
-                <li></li>
-              </ul>
-              <div class="select">
-                <div> <a href="#" class="btn-lg btn-primary btn-block1">bestellen</a> </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- /.col-sm-4  --> 
+          <?php
+        }
+        ?>
       </div>
-  	<footer>
+      <footer>
         <p>Powered by StorageGmbH</p>
       </footer>
-   </div> 
+    </div>
 
 
     <!-- Bootstrap core JavaScript
@@ -79,5 +57,5 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
     <script src="js/bootstrap.min.js"></script>
-  </body>
-</html>
+    </body>
+    </html>
