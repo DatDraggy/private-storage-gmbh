1d2727ae0195d06dd868f40e5ad77de8a4a17bfc
DatDraggy <marlinvk92@gmail.com>
2018-12-10 11:36:40 +0100

    del usr

--- Website/user_delete.php
+++ Website/user_delete.php
@@ -5,14 +5,22 @@ require_once("inc/functions.inc.php");
 
 //Überprüfe, dass der User eingeloggt ist
 //Der Aufruf von check_user() muss in alle internen Seiten eingebaut sein
-$user = check_user();
 $userId = $_SESSION['userid'];
+$targetUserId = $_GET['userid'];
+$user = check_user($userId);
 $statement = $pdo->prepare("SELECT right_id FROM user_personal WHERE user_id = :userId");
 $statement->bindParam(':userId', $userId);
 $result = $statement->execute();
 $row = $statement->fetch();
 if($statement->rowCount() === 1){
   $rightId = $row['right_id'];
+}else{die();//Keine RightID gefunden, nicht autorisiert
+}
+if($rightId <= 2){
+  //User delete, Belegte Räume befreien
+  $statement = $pdo->prepare("DELETE FROM users WHERE id = :userId");
+  $statement->bindParam(':userId', $targetUserId);
+  $statement->execute();
+  echo 'deleted';
 }
 include("templates/header.inc.php");
-?>
