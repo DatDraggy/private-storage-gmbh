c546cb484443bc021b86b147d297c29698276110
dejulas <dejulas@gmail.com>
2019-02-06 11:06:15 +0100

    x

--- Website/exportData.php
+++ Website/exportData.php
@@ -3,36 +3,27 @@ session_start();
 require_once("inc/config.inc.php");
 require_once("inc/functions.inc.php");
 require_once("inc/permissions.php");
-
+$dbConnection = buildDatabaseConnection($config);
 //Überprüfe, dass der User eingeloggt ist
 //Der Aufruf von check_user() muss in alle internen Seiten eingebaut sein
 $user = check_user();
-$userId = $_SESSION['userid'];
-$statement = $pdo->prepare("SELECT right_id FROM user_personal WHERE user_id = :userId");
-$statement->bindParam(':userId', $userId);
-$result = $statement->execute();
-$row = $statement->fetch();
-if ($statement->rowCount() === 1) {
-  $rightId = $row['right_id'];
+$rightId = $user['right_id'];
+
+try {
+  $stmt = $dbConnection->prepare('SELECT * FROM zahlungen');
+  $stmt->execute();
+  $rows = $stmt->fetchAll();
+} catch (PDOException $e) {
+  echo $e;
 }
-?>
+$csvdata = 'user_id,menge,datum' . "\n";
+foreach ($rows as $row) {
+  $csvdata .= $row['user_id'] . ',';
+  $csvdata .= $row['menge'] . ',';
+  $csvdata .= $row['datum'] . "\n";
+}
 
-<?php
-
-//$csvdata="abrechnung.csv";
 header("content-type: application/csv-tab-delimited-table");
 header("content-length: " . strlen($csvdata));
 header("content-disposition: attachment; filename=\"abrechnung.csv\"");
 echo $csvdata;
-
-
-$result = mysql_query("SELECT * FROM zahlungen", $connection);
-echo mysql_error();
-$csvdata = "TESTEINTRAG";
-while ($row = mysql_fetch_array($result)) {
-  $csvdata = $csvdata . $row["user_id"] . ";";
-  $csvdata = $csvdata . $row["menge"] . ";";
-  $csvdata = $csvdata . $row["datum"] . ";";
-}
-echo $csvdata;
-?> 
---
+++ Website/statistics.php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+require_once("inc/config.inc.php");
+require_once("inc/functions.inc.php");
+require_once("inc/permissions.php");
+require_once('templates/header.inc.php');
+
+$dbConnection = buildDatabaseConnection($config);
+$user = check_user();
+$rightId = $user['right_id'];
+
+try {
+  $stmt = $dbConnection->prepare('SELECT groesse, count(groesse) as count FROM bestellungen INNER JOIN raeume ON raeume.kennung = bestellungen.kennung WHERE aktiv = 1 GROUP BY groesse');
+  $stmt->execute();
+  $rows = $stmt->fetchAll();
+} catch (PDOException $e) {
+  echo $e;
+}
+$data = array();
+$label = array();
+foreach ($rows as $row) {
+  $data[] = $row['count'];
+  $label[] = $row['groesse'];
+}
+
+
+?>
+<script src="js/chart.min.js"></script>
+<script>
+    window.onload = function () {
+        var ctxRoomUsage = document.getElementById("roomUsage");
+        var labels = <?=json_encode($label)?>;
+        var data = <?=json_encode($data)?>;
+        var myChartRoomUsage = new Chart(ctxRoomUsage, {
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Räume',
+                    data: data,
+                    borderWidth: 1,
+                    backgroundColor: []
+                }]
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                scales: {
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Raum'
+                        }
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            maxTicksLimit: Math.min(11, 1 + Math.max.apply(null, data)),
+                            max: data.length + 1
+                        },
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'In Benutzung'
+                        }
+                    }]
+                }
+            }
+        });
+
+        const colors = ['rgba(254,218,33, 1)', 'rgba(254,49,110, 1)', 'rgba(46,52,202, 1)', 'rgba(26,251,196, 1)'];
+        do {
+            colors.some(function (color) {
+                if (myChartRoomUsage.data.datasets[0].data.length > myChartRoomUsage.data.datasets[0].backgroundColor.length) {
+                    myChartRoomUsage.data.datasets[0].backgroundColor.push(color);
+                } else {
+                    return true;
+                }
+            });
+        } while (myChartRoomUsage.data.datasets[0].data.length > myChartRoomUsage.data.datasets[0].backgroundColor.length);
+        myChartRoomUsage.update();
+    }
+</script>
+
+<div class="chart-container" style="position: relative; height:400px; width:800px">
+    <canvas id="roomUsage"></canvas>
+</div>
+
+<?php
+include("templates/footer.inc2.php")
+?>
