bcd141a78bcc0b761fc4651e97e1e6c8f5817c0a
DatDraggy <spamme@kieran.de>
2019-02-11 12:33:00 +0100

    ye

--- Website/billing.php
+++ Website/billing.php
@@ -24,8 +24,16 @@ require_once("inc/permissions.php");
 //Der Aufruf von check_user() muss in alle internen Seiten eingebaut sein
 $user = check_user();
 $userId = $_SESSION['userid'];
-
 $rightId = $user['right_id'];
+
+if(in_array($rightId, $config['administration']['userBilling']) && isset($_POST['action'])){
+  if($_POST['action'] === 'bezahlt' && !empty($_POST['abrechnungsid']))
+  {
+    $stmt = $pdo->prepare('UPDATE abrechnung SET bezahlt = 1 WHERE id = :abrechnungsid');
+    $stmt->bindParam(':abrechnungsid', $_POST['abrechnungsid']);
+    $stmt->execute();
+  }
+}
 
 include("templates/header.inc.php");
 ?>
@@ -43,7 +51,7 @@ include("templates/header.inc.php");
 
   
   <?php
-  if(in_array($user['right_id'], $config['administration']['userBilling']))
+  if(in_array($rightId, $config['administration']['userBilling']))
   {
     ?>
     <div class="panel panel-default">
@@ -67,7 +75,7 @@ include("templates/header.inc.php");
       </form>
         <?php
         //get records from database
-        $stmt = $pdo->prepare("SELECT user_id, preis, kennung FROM abrechnung");
+        $stmt = $pdo->prepare("SELECT id, user_id, preis, kennung FROM abrechnung WHERE bezahlt = 0");
         $stmt->execute();
         $rows = $stmt->fetchAll();
         $count = 1;
@@ -78,10 +86,12 @@ include("templates/header.inc.php");
             <td><?php echo $row['user_id']; ?></td>
             <td><?php echo $row['preis']; ?></td>
             <td><?php echo $row['kennung']; ?></td>
-              <td><select>
-                      <option input value="true">bezahlt</option>
-                      <option input value="false">nicht bezahlt</option>
-                  </select></td>
+            <td>
+              <form method="post">
+                <input hidden name="abrechnungsid" value="<?php echo $row['id']; ?>">
+                <button type="submit" name="action" value="bezahlt" class="edit_btn">Bezahlt</button>
+              </form>
+            </td>
           </tr>
         <?php
         }
